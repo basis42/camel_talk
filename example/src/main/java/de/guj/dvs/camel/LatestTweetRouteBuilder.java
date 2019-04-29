29367709cba7e853dda0ab91532b31a32764de4e
Mario Rutz <rutz.mario@guj.de>
2019-04-29 12:00:44 +0200

    Add local micrometer to demo

--- example/src/main/java/de/guj/dvs/camel/LatestTweetRouteBuilder.java
+++ example/src/main/java/de/guj/dvs/camel/LatestTweetRouteBuilder.java
@@ -2,17 +2,29 @@ package de.guj.dvs.camel;
 
 import org.apache.camel.Message;
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 import twitter4j.Status;
 
+@Component
 public class LatestTweetRouteBuilder extends RouteBuilder {
 
     public void configure() {
 
-        final String HASHTAG = "jconcamel";
-        String TWITTER_STREAM_API_ENDPOINT
-                = String.format("twitter-streaming:filter?keywords=%s&type=event&delay=600000&count=100&consumerKey=%s&consumerSecret=%s&accessToken=%s&accessTokenSecret=%s", HASHTAG, Props.TW_CK, Props.TW_CS, Props.TW_AT, Props.TW_ATS);
-        String S3_ENDPOINT
-                = String.format("aws-s3://rutz2-camel-talk?accessKey=%s&secretKey=%s", Props.AWS_AK, Props.AWS_SK);
+        final String HASHTAG = "gujcamel";
+        String TWITTER_STREAM_API_ENDPOINT =
+                String.format("twitter-streaming:filter?keywords=%s&type=event&delay=600000&count=100&consumerKey=%s&consumerSecret=%s&accessToken=%s&accessTokenSecret=%s",
+                HASHTAG,
+                Props.TW_CK,
+                Props.TW_CS,
+                Props.TW_AT,
+                Props.TW_ATS
+        );
+
+        String S3_ENDPOINT =
+                String.format("aws-s3://rutz2-camel-talk?accessKey=%s&secretKey=%s",
+                Props.AWS_AK,
+                Props.AWS_SK
+        );
 
         from(TWITTER_STREAM_API_ENDPOINT)
             .log("Received message containing hashtag " + HASHTAG)
@@ -30,7 +42,10 @@ public class LatestTweetRouteBuilder extends RouteBuilder {
 
             .setHeader("CamelAwsS3Key", constant("latest_tweet.html"))
             .setHeader("CamelAwsS3ContentType", constant("text/html"))
-            .to(S3_ENDPOINT);
+            .to(S3_ENDPOINT)
+
+            .to("micrometer:counter:tweet.received")
+        ;
     }
 
 }
--- example/src/main/java/de/guj/dvs/camel/MainApp.java
+++ example/src/main/java/de/guj/dvs/camel/MainApp.java
@@ -1,20 +1,67 @@
 package de.guj.dvs.camel;
 
-import org.apache.camel.main.Main;
+import io.micrometer.core.instrument.Clock;
+import io.micrometer.core.lang.Nullable;
+import io.micrometer.graphite.GraphiteConfig;
+import io.micrometer.graphite.GraphiteMeterRegistry;
+import io.micrometer.graphite.GraphiteProtocol;
+import org.apache.camel.component.micrometer.MicrometerConstants;
+import org.apache.camel.spring.javaconfig.CamelConfiguration;
+import org.apache.camel.spring.javaconfig.Main;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
 
 /**
  * A Camel Application
  */
-public class MainApp {
+@Configuration
+@ComponentScan
+public class MainApp extends CamelConfiguration {
 
     /**
      * A main() so we can easily run these routing rules in our IDE
      */
     public static void main(String... args) throws Exception {
         Main main = new Main();
-        main.addRouteBuilder(new LatestTweetRouteBuilder());
-        main.run(args);
+        main.setConfigClass(MainApp.class);
+        main.run();
     }
 
+
+    /**
+     * Set up registry. When using Spring Boot, this is provided for you, and you have to provide
+     * a bean of type MeterRegistryCustomizer in order to apply common tags, filters, etc.
+     */
+    @Bean(name = MicrometerConstants.METRICS_REGISTRY_NAME)
+    public GraphiteMeterRegistry meterRegistry() {
+
+        GraphiteConfig graphiteConfig = new GraphiteConfig() {
+            @Override
+            public Duration step() {
+                return Duration.ofSeconds(5);
+            }
+
+            @Override
+            public GraphiteProtocol protocol() {
+                return GraphiteProtocol.PLAINTEXT;
+            }
+
+            @Override
+            public int port(){
+                return 2003;
+            }
+
+            @Override
+            @Nullable
+            public String get(String k) {
+                return null;
+            }
+        };
+
+        return new GraphiteMeterRegistry(graphiteConfig, Clock.SYSTEM);
+    }
 }
 
